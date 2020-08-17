cc328eb8decfc48f5d0952dc79702b1de84b5264
Meng <you@example.com>
2020-08-17 14:19:51 -0400

    day 1 setup

--- public/products.js
+++ public/products.js
@@ -8,17 +8,24 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/products', async (_request, response) => {
-  const products = await stripe.products.list({ limit: 100 });
-
-  const prices = await stripe.prices.list({ limit: 100 });
-
+  // Ask stripe for all of the products
+  const products = await stripe.products.list({
+    limit: 100,
+  });
+  // Ask stripe for all of the prices
+  const prices = await stripe.prices.list({
+    limit: 100,
+  });
+  // Associate the products with the prices
   prices.data.forEach(price => {
+    // find the product associated with this price.
     const theAssociatedProduct = products.data.find(
       product => product.id === price.product
     );
+    // associate the product with the price
     theAssociatedProduct.price = price;
   });
-
+  // Send back a list of all the products and their associated prices
   const cleanedUpProducts = products.data.map(product => ({
     name: product.name,
     description: product.description,
@@ -28,7 +35,6 @@ app.get('/products', async (_request, response) => {
     price_cents: product.price.unit_amount,
     price_id: product.price.id,
   }));
-
   response.json(cleanedUpProducts);
 });
 
